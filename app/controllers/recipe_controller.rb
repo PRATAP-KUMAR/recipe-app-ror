ce7b3baf703471b4a7290130145e1690831a2bd9
PRATAP-KUMAR <pratap@fastmail.fm>
2022-12-15 10:40:58 +0530

    Edit routes for fixing route paths

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -7,4 +7,12 @@ class RecipeController < ApplicationController
     @recipe = Recipe.find(params[:id])
     @foods = current_user.foods
   end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  # def create
+  #   @recipe = Recipe.new()
+  # end
 end
