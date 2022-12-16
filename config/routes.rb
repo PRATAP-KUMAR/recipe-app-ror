bf903d8fc9296686a15cc96d5564071ccf10dbae
PRATAP-KUMAR <pratap@fastmail.fm>
2022-12-16 22:18:07 +0530

    Update toggle check box for public

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'public_recipies', to: 'recipe#public_recipe'
   resources :user, only: [:index] do
     resources :food, only: [:index, :new, :create, :destroy]
-    resources :recipe, only: [:index, :show, :new, :create, :destroy ] do
+    resources :recipe, only: [:index, :show, :new, :create, :destroy, :edit ] do
       resources :recipe_food, only: [:new, :edit, :create, :update, :destroy]
       end 
   end
